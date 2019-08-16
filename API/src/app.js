5730bc9b855451a643a5299ac47c58e5e9aa4535
Elmer Alay <alayelmer1993@gmail.com>
2019-08-16 11:31:31 -0600

    agregadas las rutas de municipio al archivo app.js

--- API/src/app.js
+++ API/src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { json } = require('express');
 
+const muniRoutes = require('./routes/municipio_routes');
+
 const app = express();
 
 //middlewares
 app.use(json());
 
 //routes
+app.use("/municipio", muniRoutes);
 
 module.exports = app;
