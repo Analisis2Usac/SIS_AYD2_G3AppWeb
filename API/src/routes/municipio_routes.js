2bcf77c88cf1653aee2d3042d955288fb49560b1
Elmer Alay <alayelmer1993@gmail.com>
2019-08-16 11:29:19 -0600

    agregadas las rutas de municipio

--- API/src/routes/municipio_routes.js
+++ API/src/routes/municipio_routes.js
@@ -2,5 +2,14 @@ const express = require('express');
 const router = express.Router();
 const muni_controller = require('../controllers/muni_controller');
 
+router.get("/", muni_controller.get);
+
+router.get("/:id", muni_controller.getOne);
+
+router.post("/", muni_controller.insert);
+
+router.put("/:id", muni_controller.update);
+
+router.delete("/:id", muni_controller.deleteMuni);
 
 module.exports = router;
