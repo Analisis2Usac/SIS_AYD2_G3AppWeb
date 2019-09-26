750a9de4772f28509dac22659c0b04ab6bbe60f5
jddr27 <jddeleonr.inf2013@gmail.com>
2019-09-25 22:53:42 -0600

    Probando para actualizarse

--- API/src/app.js
+++ API/src/app.js
@@ -8,6 +8,14 @@ const comenRoutes = require('./routes/comentario_routes');
 const caliRoutes = require('./routes/calificacion_routes');
 const formaRoutes = require('./routes/forma_routes');
 const detalleRoutes = require('./routes/detalle_routes');
+const categoryRoutes = require('./routes/category_routes');
+const serviceRoutes = require('./routes/service_routes');
+const listserviceRoutes = require('./routes/list-service_routes');
+const subscriptionRoutes = require('./routes/subscription_routes');
+const empresaRoutes = require('./routes/empresa_routes');
+const empleadoRoutes = require('./routes/empleado_routes');
+const photoRoutes = require('./routes/photo_routes');
+const docRoutes = require('./routes/doc_routes');
 
 const app = express();
 
@@ -22,5 +30,13 @@ app.use("/comentario", comenRoutes);
 app.use("/calificacion", caliRoutes);
 app.use("/formapago", formaRoutes);
 app.use("/detalle", detalleRoutes);
+app.use("/categoria", categoryRoutes);
+app.use("/servicio", serviceRoutes);
+app.use("/lista-servicio", listserviceRoutes);
+app.use("/suscripcion", subscriptionRoutes);
+app.use("/empresa", empresaRoutes);
+app.use("/empleado", empleadoRoutes);
+app.use("/foto", photoRoutes);
+app.use("/documento", docRoutes);
 
 module.exports = app;
