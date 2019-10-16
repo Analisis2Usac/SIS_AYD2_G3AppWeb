8e2f094cb3a41574100b77a2afaf0399fd54129b
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2019-10-16 15:51:19 -0600

    Mostrando la vista principal!

--- PROYECTO_SIS/src/sitio.js
+++ PROYECTO_SIS/src/sitio.js
@@ -1,31 +1,12 @@
-const express=require('express');
-const app=express();
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require('express');
+const router = express.Router();
 
-
-app.set('host','0.0.0.0');
-app.set('port','3000');
-
-//settings
-app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('html', require('ejs').renderFile);//decimos que usaremos la sintaxis y extension html , pero con el motor de plantillas ejs incluido
-app.set ('views',path.join(__dirname,'views'));//establecemos la ubicacion de la carpeta view de nuestro proyecto
-app.set('view engine', 'ejs');  //decimos que usaremos el motor de plantillas ejs
-
-
-
-//static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-//Rutas
-app.use(require('./routes/routes'));
-
-
-app.listen(app.get('port'),app.get('host'),()=>{
-console.log(`Running on http://${app.get('host')}:${app.get('port')}`);
-//console.log(__dirname);
+router.get('/', (req, res) => {
+    res.send('hola');
 });
+
+
+module.exports = router;
+
+
+
