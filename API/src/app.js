2b91a08753499247115cffb45c07249938e34e1f
jorged104 <nowelljorge@gmail.com>
2019-10-16 22:11:08 -0600

    Revert "Develop brandon vistas"

--- API/src/app.js
+++ API/src/app.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const { json } = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-
-
-
 
 const muniRoutes = require('./routes/municipio_routes');
 const usuRoutes = require('./routes/usuario_routes');
@@ -21,18 +16,8 @@ const empresaRoutes = require('./routes/empresa_routes');
 const empleadoRoutes = require('./routes/empleado_routes');
 const photoRoutes = require('./routes/photo_routes');
 const docRoutes = require('./routes/doc_routes');
-const sitioWeb = require('./routes/sitio_routes');
 
 const app = express();
-//settings
-app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('html', require('ejs').renderFile);//decimos que usaremos la sintaxis y extension html , pero con el motor de plantillas ejs incluido
-app.set ('views',path.join(__dirname,'views'));//establecemos la ubicacion de la carpeta view de nuestro proyecto
-app.set('view engine', 'ejs');  //decimos que usaremos el motor de plantillas ejs
-
-//static files
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 //middlewares
 app.use(json());
@@ -53,6 +38,5 @@ app.use("/empresa", empresaRoutes);
 app.use("/empleado", empleadoRoutes);
 app.use("/foto", photoRoutes);
 app.use("/documento", docRoutes);
-app.use(require('./routes/sitio_routes'));
 
 module.exports = app;
