5f5c3c5c231e8ee122105a66ab4a8e0e0ae66a9d
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2019-10-31 16:57:57 -0600

    Login y Registro 75%Funcional

--- API/src/routes/sitio_routes.js
+++ API/src/routes/sitio_routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const sitio_controller = require('../controllers/sitio_controller');
+const cargar_vistas = require('../controllers/carga_datos_a_vista');
 
 
 router.get('/', (req, res) => {
@@ -7,21 +9,39 @@ router.get('/', (req, res) => {
 });
 
 
+router.get('/prueba', (req, res) => {
+    
+    
+    console.log(process.env.HOST);
+    console.log(process.env.PORT);
+});
+
+
+
 router.get('/login', (req, res) => {
     res.render('login.html');
 });
 
 
+router.get('/register-ind', (req, res) => {
+    res.render('registration-individual.html');
+});
+
+router.get('/catalog-page', (req, res) => {
+    res.render('catalog-page.html');
+});
+
+router.get('/category-page',cargar_vistas.MostrarCategoria);
+
+router.get('/register-comp', (req, res) => {
+    res.render('registration-company.html');
+});
+
 router.get('/registration', (req, res) => {
     res.render('registration.html');
 });
 
+router.post('/createRegister',sitio_controller.crearRegistro);
 
-router.post('/createRegister', (req, res) => {
-    var email=req.body.email;
-    var password=req.body.password;
-    var passwordconfirm=req.body.passwordconfirm;
-
-    res.send(`Correo:${email},Password:${password},PasswordRepeat:${passwordconfirm}`);
-});
 module.exports = router;
+
